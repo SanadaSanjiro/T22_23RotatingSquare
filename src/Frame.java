40865284161a6cfa4d9f9baf47748b42b4de2913
Alexandr Pogodaev <trollegon@narod.ru>
2024-03-22 12:12:44 +0800

    Autorotation added

--- src/Frame.java
+++ src/Frame.java
@@ -3,9 +3,11 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class Panel extends JPanel {
-    private int rotationFactor = 100;
+    private int rotationFactor;
     private int scaleFactor = 100;
     private int degree = 0;
     private int[][] vertices;
@@ -21,24 +23,33 @@ class Panel extends JPanel {
             side = Integer.min(maxHeight, maxWidth);
             square = SquareProducer.getSquare((int) (side * .25), (int) (side * .25), (int) (side * .5), Color.GREEN);
             //square = LineProducer.getLine((int) (side * .25), (int) (side * .25), (int) (side * .5), Color.GREEN);
+            RotatingThread rt = new RotatingThread(this);
+            ExecutorService es = Executors.newCachedThreadPool();
+            es.execute(rt);
         }
         Plotter.plot(g, square);
     }
 
-    public void rotate(int angle) {
-            Rotator.rotate(square, angle);
+    public void rotate() {
+            Rotator.rotate(square, 1*rotationFactor);
             repaint();
+    }
+
+    public int getRotationFactor() {
+        return rotationFactor;
+    }
+    public void setRotationFactor(int rf) {
+        rotationFactor = rf;
     }
 }
 
 public class Frame extends JFrame {
     private Panel panel = new Panel();
-    private JSlider adjustCycles = new JSlider(-90, 90, 0);
+    private JSlider adjustCycles = new JSlider(-30, 30, 0);
     public Frame() {
         adjustCycles.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
-                panel.rotate(
-                        ((JSlider)e.getSource()).getValue());
+                panel.setRotationFactor(((JSlider)e.getSource()).getValue());
             }
         });
         add(panel);
---
+++ src/RotatingThread.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.TimeUnit;
+
+public class RotatingThread implements Runnable{
+    Panel panel;
+    RotatingThread(Panel p) {
+        panel = p;
+    }
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(40 );
+                panel.rotate();
+            } catch (InterruptedException e) {
+                System.out.println("Работа прервана");
+            }
+        }
+    }
+}
