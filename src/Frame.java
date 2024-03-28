5c5983ce90438916f2fad373fc4209ac825d1705
Alexandr Pogodaev <trollegon@narod.ru>
2024-03-28 18:07:12 +0800

    Resizing job started

--- src/Frame.java
+++ src/Frame.java
@@ -9,8 +9,6 @@ import java.util.concurrent.Executors;
 class Panel extends JPanel {
     private int rotationFactor;
     private int scaleFactor = 100;
-    private int degree = 0;
-    private int[][] vertices;
     private int maxWidth, maxHeight, side;
 
     Shape square;
@@ -25,13 +23,13 @@ class Panel extends JPanel {
             //square = LineProducer.getLine((int) (side * .25), (int) (side * .25), (int) (side * .5), Color.GREEN);
             RotatingThread rt = new RotatingThread(this);
             ExecutorService es = Executors.newCachedThreadPool();
-            es.execute(rt);
+            //es.execute(rt);
         }
         Plotter.plot(g, square);
     }
 
     public void rotate() {
-            Rotator.rotate(square, 1*rotationFactor);
+            square.rotate(rotationFactor);
             repaint();
     }
 
@@ -41,18 +39,39 @@ class Panel extends JPanel {
     public void setRotationFactor(int rf) {
         rotationFactor = rf;
     }
+
+    public int getScaleFactor() {
+        return scaleFactor;
+    }
+
+    public void setScaleFactor(int scaleFactor) {
+        this.scaleFactor = scaleFactor;
+        square.resize(scaleFactor);
+        repaint();
+    }
 }
 
 public class Frame extends JFrame {
     private Panel panel = new Panel();
     private JSlider adjustCycles = new JSlider(-30, 30, 0);
+    private JSlider adjustScale = new JSlider(JSlider.VERTICAL, 0, 99, 0);
     public Frame() {
         adjustCycles.addChangeListener(new ChangeListener() {
+            @Override
             public void stateChanged(ChangeEvent e) {
                 panel.setRotationFactor(((JSlider)e.getSource()).getValue());
+                System.out.println("Rotation factor: " + panel.getRotationFactor());
+            }
+        });
+        adjustScale.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                panel.setScaleFactor(((JSlider)e.getSource()).getValue());
+                System.out.println("Scale factor: " + panel.getScaleFactor());
             }
         });
         add(panel);
         add(BorderLayout.SOUTH, adjustCycles);
+        add(BorderLayout.EAST, adjustScale);
     }
 }
--- src/Shape.java
+++ src/Shape.java
@@ -3,42 +3,83 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Shape {
-    private Color color;
-    private Set<Vertice> vertices = new HashSet<>();
-    private Set<Edge> edges = new HashSet<>();
-    private Coordinates centRot;
+    private Color color; // цвет
+    private Set<Vertice> vertices = new HashSet<>(); // грани фигуры
+    private Set<Edge> edges = new HashSet<>(); // узлы фигуры
+    private Coordinates centRot = new Coordinates(0,0); // координаты центра вращения
+    private int angle; //угол наклона фигуры в градусах
+    private int scale=100; // масштаб в процентах
 
-    public Color getColor() {
+    public synchronized Color getColor() {
         return color;
     }
 
-    public void setColor(Color color) {
+    public synchronized void setColor(Color color) {
         this.color = color;
     }
 
-    public Set<Vertice> getVertices() {
+    public synchronized Set<Vertice> getVertices() {
         return vertices;
     }
 
-    public void addVertice(Vertice vertice) {
+    public synchronized void addVertice(Vertice vertice) {
         vertices.add(vertice);
     }
 
-    public Set<Edge> getEdges() {
+    public synchronized Set<Edge> getEdges() {
         return edges;
     }
 
-    public void addEdge(Edge edge) {
+    public synchronized void addEdge(Edge edge) {
         if (vertices.contains(edge.getVertice1()) && vertices.contains(edge.getVertice2())) {
             edges.add(edge);
         } else throw new IllegalArgumentException("Вершины данной грани не принадлежат фигуре!");
     }
 
-    public void setCentRot(Coordinates c) {
+    public synchronized void setCentRot(Coordinates c) {
         centRot = c;
     }
 
-    public Coordinates getCentRot() {
+    public synchronized Coordinates getCentRot() {
         return centRot;
+    }
+
+    public synchronized void rotate(int a) {
+        angle = angle % 360 + a;
+        System.out.println("Новый угол: " + angle);
+        vertices.forEach(v-> {
+            Coordinates c = v.getCoordinates();
+            // перемещаем центр вращения в начало отсчета
+            double x = c.getX() - centRot.getX();
+            double y = c.getY() - centRot.getY();
+            // расчитываем новые координаты относительно 0;0
+            double x1 = x * Math.cos(a*Math.PI/180.0) - y * Math.sin(a*Math.PI/180.0);
+            double y1 = x * Math.sin(a*Math.PI/180.0) + y * Math.cos(a*Math.PI/180.0);
+            // возвращаем центр координат на место
+            x1 = x1 + centRot.getX();
+            y1 = y1 + centRot.getY();
+            v.setCoordinates(new Coordinates(x1,y1));
+        });
+    }
+
+    public synchronized void resize(int newSize) {
+        int scaleChange = scale - newSize;
+        scale = newSize;
+        System.out.println("Новый масштаб: " + scale);
+        vertices.forEach(v-> {
+            Coordinates c = v.getCoordinates();
+            // перемещаем центр вращения в начало отсчета
+            double x = c.getX() - centRot.getX();
+            double y = c.getY() - centRot.getY();
+            // расчитываем новые координаты относительно 0;0
+            double x1 = x * (100 - scaleChange)/100;
+            double y1 = y * (100 - scaleChange)/100;
+            System.out.println(v +  " :x1 = " + x1 + ", y1 = " + y1);
+            // возвращаем центр координат на место
+            x1 = x1 + centRot.getX();
+            y1 = y1 + centRot.getY();
+            System.out.println("x1 = " + x1 + ", y1 = " + y1);
+            v.setCoordinates(new Coordinates(x1,y1));
+        });
     }
 }
